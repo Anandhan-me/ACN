a2e866419a95265a6a46d7ba11330e55583a3fc9
Anandakrishnan <anandakrishnananish1@gmail.com>
2026-02-23 14:43:21 +0530

    Add files via upload

---
+++ oddeven.sh
@@ -0,0 +1,12 @@
+oddoreven(){
+if [ $(($number % 2)) -eq 0 ]
+then 
+echo "$number is even"
+else
+echo "$number is odd"
+fi
+}
+echo "please enter a number :"
+read number 
+oddoreven "$number"
+    
